af430128d69ccd9c8cb1c1c67bf54f44b3d7ad5c
ruhsane <ruhsane924@gmail.com>
2018-10-19 00:06:27 -0700

    stores profile informations to firebase storage and database

--- Seven/CreateProfileViewController.swift
+++ Seven/CreateProfileViewController.swift
@@ -8,18 +8,34 @@
 
 import Foundation
 import UIKit
+import Firebase
+
 
 class CreateProfileViewController: UIViewController {
     
     @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    @IBOutlet weak var doneButton: UIButton!
+    
+    
     var imagePicker: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(openImagePicker))
+        doneButton.addTarget(self, action: #selector(handleProfileData), for: .touchUpInside)
+
+        //dismiss keyboard when tapped outside of the textfield
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
+        self.view.addGestureRecognizer(tapGesture)
+        
+        //open image picker when clicked
+        let imageTap = UITapGestureRecognizer(target: self, action: #selector(openImagePicker(_:)))
         profileImage.isUserInteractionEnabled = true
-        profileImage.addGestureRecognizer(tapGesture)
+        profileImage.addGestureRecognizer(imageTap)
         
         imagePicker = UIImagePickerController()
         imagePicker.allowsEditing = true
@@ -27,12 +43,109 @@ class CreateProfileViewController: UIViewController {
         imagePicker.delegate = self
         
     }
+    func errorMessage(){
+        // create the alert
+        let alert = UIAlertController(title: "Please Complete Filling Out The Profile", message: "Please Complete Filling Out The Profile", preferredStyle: UIAlertController.Style.alert)
+        
+        // add an action (button)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        // show the alert
+        self.present(alert, animated: true, completion: nil)
+    }
     
+    @objc func handleProfileData(){
+        guard let name = nameTextField.text else{return errorMessage()}
+        guard let age = ageTextField.text else{return errorMessage()}
+        guard let description = descriptionTextField.text else{return errorMessage()}
+        guard let image = profileImage.image else{return errorMessage()}
+        
+        //store the profile image to firebase storage
+//        self.uploadProfileImage(image, url)
+        
+        self.uploadProfileImage(image) { url in
+            
+            if url != nil {
+                let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                changeRequest?.photoURL = url
+                
+                changeRequest?.commitChanges { error in
+                    if error == nil {
+                        print("User picture changed!")
+                        
+                        self.saveProfile(name:name, age:age, description:description, profileImageURL: url!) { success in
+                            if success {
+                                self.dismiss(animated: true, completion: nil)
+                            }
+                        }
+                        
+                    } else {
+                        print("Error: \(error!.localizedDescription)")
+                    }
+                }
+            } else {
+                // Error unable to upload profile image
+            }
+            
+        }
+        
+    }
+    
+    func saveProfile(name:String, age:String, description:String, profileImageURL: URL, completion: @escaping ((_ success:Bool)->())){
+        //store the name, age and description to databse
+
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let databaseRef = Database.database().reference().child("Users").child(uid)
+        
+        let userObject = [
+            "Name": name,
+            "Age": age,
+            "Description": description,
+            "photoURL": profileImageURL.absoluteString
+            ] as [String:Any]
+        
+        databaseRef.updateChildValues(userObject) { error, ref in
+            completion(error == nil)
+        
+        }
+    }
     
     @objc func openImagePicker(_ sender: Any){
         //OPEN IMAGE PICKER
         print("tapped")
         self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @objc func dismissKeyboard (_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        ageTextField.resignFirstResponder()
+        descriptionTextField.resignFirstResponder()
+
+    }
+    
+    func uploadProfileImage(_ image: UIImage, completion: @escaping ((_ url:URL?)->())) {
+        guard let uid = Auth.auth().currentUser?.uid else {return}
+        let storageRef = Storage.storage().reference().child("Users").child(uid)
+        
+        guard let imageData = image.jpegData(compressionQuality: 0.75) else { return }
+        
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpg"
+        
+        storageRef.putData(imageData, metadata: metaData) { metaData, error in
+            if error == nil, metaData != nil {
+                storageRef.downloadURL(completion: { (url, error) in
+                    print(url)
+                    if error != nil {
+                        //failed
+                        completion(nil)
+                    } else {
+                        //success
+                        completion(url?.absoluteURL)
+                    }
+                })
+            }
+        }
     }
     
 }
